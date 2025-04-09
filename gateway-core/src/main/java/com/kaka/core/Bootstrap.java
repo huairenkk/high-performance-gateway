dbdf6a85e78d268f435b48eda28f2350ceaa99e9
全洪江 <1797594909@qq.com>
2025-04-09 17:01:40 +0800

    项目初始化

---
+++ gateway-core/src/main/java/com/kaka/core/Bootstrap.java
@@ -0,0 +1,21 @@
+package com.kaka.core;
+
+
+/**
+ * API网关启动类
+ */
+public class Bootstrap {
+    public static void main(String[] args) {
+        //加载网关核心静态配置
+
+        //插件初始化
+
+        //配置中心管理器初始化，连接配置中心，监听配置中心的新增、修改、删除
+
+        //初始化容器
+
+        //连接注册中心，将注册中心的实例加载到本地
+
+        //服务优雅关机
+    }
+}
